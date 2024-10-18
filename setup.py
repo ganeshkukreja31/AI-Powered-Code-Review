8a46ecce6df40acdd45032665d2b024d390573d4
Ganesh Kukreja <kukreja.g@northeastern.edu>
2024-10-18 17:19:07 -0400

    import statment added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generavtive AI Project',
+    version = '0.0.0',
+    author = 'Ganesh Kukreja',
+    author_email = 'kukreja.g@northeastern.edu',
+    packages = find_packages(),
+    install_requires = []
+)
